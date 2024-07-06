26f538fa26e8a6deaff4e652eb3d2fabaefdb785
AntCannon <anthonycannonier@gmail.com>
2024-07-06 12:23:19 -0400

    Add index route
    1. Add transaction.model data
    2. Add index route in transactions.controller
    3. Add config transaction resource in app.js

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const app = express()
 
+// controllers
+const transactionsController = require('./controllers/transactions.controller')
+
 // health check
 app.get('/', (req, res) => {
   res.send('Hello, world!')
 })
+
+// resources
+app.use('/transactions', transactionsController)
 
 module.exports = app;
